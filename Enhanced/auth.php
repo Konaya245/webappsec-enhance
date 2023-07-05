7fbce24d547c14f79e2688d0641ffa9dfb24c5a2
hanisyamira <hanisyamira11@gmail.com>
2023-07-05 17:36:17 +0800

    updated auth.php

--- Enhanced/auth.php
+++ Enhanced/auth.php
@@ -9,7 +9,7 @@
         $email = mysqli_real_escape_string($con, $email);  
         $password = mysqli_real_escape_string($con, $password);  
       
-        $sql = "select email, password from register where email = '$email' and password = '$password'";  
+        $sql = "select email, password from register where email = '$email' and password = '" . sha1($password) . "'";  
         $result = mysqli_query($con, $sql);  
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
