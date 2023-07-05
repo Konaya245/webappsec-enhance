55bbdebfb8877ecf9a1b251997d7393c078d84c8
hanisyamira <hanisyamira11@gmail.com>
2023-07-05 17:48:43 +0800

    updatedd login.php

--- Enhanced/login.php
+++ Enhanced/login.php
@@ -89,13 +89,13 @@ hr {
                 var id=document.f1.user.value;  
                 var ps=document.f1.pass.value;  
                 if(id.length=="" && ps.length=="") {  
-                    alert("User Name and Password fields are empty");  
+                    alert("Email and Password fields are empty");  
                     return false;  
                 }  
                 else  
                 {  
                     if(id.length=="") {  
-                        alert("User Name is empty");  
+                        alert("Email is empty");  
                         return false;  
                     }   
                     if (ps.length=="") {  
