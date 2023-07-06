dc8127236984c39c9bf3478b7b3dbce48cca0501
HaniHairul <hanihairulazam@gmail.com>
2023-07-06 11:56:40 +0800

    update session.php

--- Enhanced/session.php
+++ Enhanced/session.php
@@ -1,7 +1,11 @@
 <?php
 ini_set('session.gc_maxlifetime', 1800); // Set session timeout to 30 minutes
 session_start();
+<<<<<<< Updated upstream
 
 header("Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;");
 
+=======
+
+>>>>>>> Stashed changes
 ?>
