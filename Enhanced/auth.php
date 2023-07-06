1ad07cf7f05b1d1b83dee7fb10c096ebdc91929d
Konaya245 <mzaidi.ar@gmail.com>
2023-07-07 01:42:54 +0800

    CSRF token, HSTS and X-Content-Type-Options added
    if break code try remove HSTS or csrf lines

--- Enhanced/auth.php
+++ Enhanced/auth.php
@@ -20,11 +20,22 @@
 			$_SESSION['email'] = $email;
 			
 			// Generate a random session ID
-			$randomBytes = random_bytes(32); // Adjust the byte length as needed
-			$generatedSessionID = bin2hex($randomBytes); // Convert the random bytes to a hexadecimal string
+			$randomValue = random_bytes(32); // Cryptographic random value
+			$generatedSessionID = bin2hex($randomValue); // Convert the random bytes to a hexadecimal string
 
 			// Set the session ID
 			session_id($generatedSessionID);
+			
+			// Create the CSRF Token
+			$secret = getenv("CSRF_SECRET"); // HMAC secret key
+			$sessionID = session_id(); // prev generated user session ID
+			$randomValue2 = bin2hex(random_bytes(64)); // Cryptographic random value
+			$message = $sessionID . "!" . $randomValue2; // HMAC message payload
+			$hmac = hash_hmac("sha256", $message, $secret); // Generate the HMAC hash
+			$csrfToken = $hmac . "." . $message; // Combine HMAC hash with message to generate the token
+
+			// Store the CSRF Token in a cookie
+			setcookie("csrf_token", $csrfToken, 0, "/", "", true, true); // Set cookie with secure and HttpOnly flags
         }  
         else{  
              echo "<h1> Login failed. Please try again!</h1>";  
