3dae904a9ffaee27029e4b068558643167244e46
Konaya245 <mzaidi.ar@gmail.com>
2023-07-07 01:49:00 +0800

    Update CSP
    home.php event button not showing

--- Enhanced/session.php
+++ Enhanced/session.php
@@ -2,7 +2,7 @@
 ini_set('session.gc_maxlifetime', 1800); // Set session timeout to 30 minutes
 session_start();
 
-header("Content-Security-Policy: default-src 'self' *.google.com ; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com/ ; img-src 'self' data:; font-src 'self' https://cdnjs.cloudflare.com/ https://fonts.gstatic.com ;");
+header("Content-Security-Policy: default-src 'self' *.google.com ; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com/ ; img-src 'self' data:; font-src 'self' https://cdnjs.cloudflare.com/ https://fonts.gstatic.com ;");
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
 header("X-Content-Type-Options: nosniff");
 ?>
